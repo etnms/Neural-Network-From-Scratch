bf1ebdaea39a8924ed2cde5e6c4a838ac7e9117a
etnms <etiennenemes@gmail.com>
2023-10-28 09:11:45 -0400

    first commit, added base layer

---
+++ layer/layer.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+'''
+Base class for dense layers
+'''
+
+
+class LayerDense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.1 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+    def backward(self):
+        pass
