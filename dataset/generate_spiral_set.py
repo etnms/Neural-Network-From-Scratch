e3822a0c3b51caf392c8a13475cba052f30b128b
etnms <etiennenemes@gmail.com>
2023-10-31 14:32:07 -0400

    added panda to read .csv files, move spiral creation function to dataset folder, started working on reading the .csv file

---
+++ dataset/generate_spiral_set.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+'''
+Generates a spiral data set
+'''
+
+np.random.seed(0)
+
+# generate data for learning and testing purposes
+# Generate spirals with numbers of points and number of classes
+def create_data(points, classes):
+    X = np.zeros((points*classes, 2))
+    y = np.zeros(points*classes, dtype='uint8')
+    for class_number in range(classes):
+        ix = range(points * class_number, points*(class_number+1))
+        r = np.linspace(0.0, 1, points) # radius
+        t = np.linspace(class_number*4, (class_number+1) * 4, points) + np.random.randn(points) * 0.2
+        X[ix] = np.c_[r*np.sin(t*2.5), r*np.cos(t*2.5)]
+        y[ix] = class_number
+    return X, y
+
+#X, y = create_data(100, 3)
--- testing.py
+++ testing.py
@@ -1,24 +1,20 @@
 import numpy as np
+import pandas as pd
 from epoch import epoch_function
 from create_layer import CreateLayer
 
 
-np.random.seed(0)
+data = pd.read_csv('./dataset/random_int.csv', encoding='utf-8', dtype = str)  
 
-# generate data for learning and testing purposes
-# Generate spirals with numbers of points and number of classes
-def create_data(points, classes):
-    X = np.zeros((points*classes, 2))
-    y = np.zeros(points*classes, dtype='uint8')
-    for class_number in range(classes):
-        ix = range(points * class_number, points*(class_number+1))
-        r = np.linspace(0.0, 1, points) # radius
-        t = np.linspace(class_number*4, (class_number+1) * 4, points) + np.random.randn(points) * 0.2
-        X[ix] = np.c_[r*np.sin(t*2.5), r*np.cos(t*2.5)]
-        y[ix] = class_number
-    return X, y
+# Remove invalid rows
+#data = data.dropna(subset=['Int_value'])
 
-X, y = create_data(100, 3)
+# Shuffle the data if sample not shuffled already
+#data = data.sample(frac=1).reset_index(drop=True)
+
+X = data['Int_value']
+y = data['Size']
+
 
 # Hyperparameters
 learning_rate = 0.1
@@ -28,8 +24,8 @@ batch_size = 1
 # Number of classes = number of inputs
 # In case of second layer until the end it takes the number of neurons from the previous layer
 # Ex layer1(2,512), layer2(512,3) layer3(3, 128) etc.
-layer1 = CreateLayer.create(number_classes=2, number_neurons=512, activation_function='relu')
-layer2 = CreateLayer.create(number_classes=512, number_neurons=3, activation_function='softmax')
+layer1 = CreateLayer.create(number_classes=1, number_neurons=512, activation_function='relu')
+layer2 = CreateLayer.create(number_classes=512, number_neurons=1, activation_function='softmax')
 layers=[layer1, layer2]
 
 layer_dense_list = [layer[0] for layer in layers]
