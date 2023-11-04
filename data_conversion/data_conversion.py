cd44ec5f2e92c5cd4ca0d103444531c7421541ab
etnms <etiennenemes@gmail.com>
2023-11-04 10:49:07 -0400

    added some data conversion functions (mean-max, standardization)

---
+++ data_conversion/data_conversion.py
@@ -0,0 +1,19 @@
+import string
+
+
+'''
+Functions to convert data to readable format for the network
+'''
+
+# Mean max function 
+def mean_max_scaling(data, column_name: string):
+    min_value = data[column_name].min()
+    max_value = data[column_name].max()
+    return data[column_name].apply(lambda x: (x - min_value) / (max_value - min_value))
+
+
+# Standardization function 
+def standardization(data, column_name: string):
+    mean_value = data[column_name].mean()
+    std_dev = data[column_name].std()
+    return data[column_name].apply(lambda x: (x - mean_value) / std_dev)
