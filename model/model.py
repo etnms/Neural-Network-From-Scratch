c258866cc3766267644106eee08b1c79841fd0a8
etnms <etiennenemes@gmail.com>
2023-11-07 13:00:54 -0500

    improved structure and function names using the model class instead of a name like epoch_funtion

--- model/model.py
+++ model/model.py
@@ -1,21 +1,94 @@
+from loss import LossCategoricalCrossentropy
+import numpy as np
+import pandas as pd
+
+
 '''
-Model class with save and load functions to save and load a model that has been created.
+Model class with train and testing functions. 
+It also has a forward_backward function for the training, while the testing only uses a forward pass as it doesn't require
+to change the model.
+
+To implement: Save and load functions to save and load a model that has been created.
 '''
+
 
 class Model:
-    def __init__(self, layer1, layer2, activation1, activation2):
-        self.layer1 = layer1
-        self.layer2 = layer2
-        self.activation1 = activation1
-        self.activation2 = activation2
+    # def forward_and_backward_pass(layers, activations, layer1, activation1, layer2, activation2, data_X, data_y, learning_rate):
+    def forward_and_backward_pass(self, layers, activations, data_X, data_y, learning_rate):
+        x = data_X
 
-    def save_model(self):
-        model_data = {
-        'layer1_weights': self.layer1.weights,
-        'layer1_biases': self.layer1.biases,
-        'layer2_weights': self.layer2.weights,
-        'layer2_biases': self.layer2.biases,
-        'activation1_state': self.activation1.__dict__,
-        'activation2_state': self.activation2.__dict__,
-        }
-        print(model_data)
+        # Forward pass
+        for layer, activation in zip(layers, activations):
+            layer.forward(x)
+            activation.forward(layer.output)
+            x = activation.output
+
+        # Calculate the loss
+        loss_function = LossCategoricalCrossentropy()
+        loss = loss_function.calculate(x, data_y)
+
+        # Backward pass
+        loss_function.backward(x, data_y) #activations[-1].output = x, could be used as well since starting from last
+        dvalues = loss_function.dvalues
+
+        for layer, activation in zip(reversed(layers), reversed(activations)):
+            activation.backward(dvalues)
+            layer.backward(activation.dvalues)
+            dvalues = layer.dvalues
+
+        # Optimization step
+        for layer in layers:
+            layer.weights -= learning_rate * layer.dweights
+            layer.biases -= learning_rate * layer.dbiases
+
+        return loss, x
+
+    def train_model(self, layers, activations, num_epochs, batch_size, learning_rate, data_X, data_y):
+        data_size = len(data_X)
+
+        for epoch in range(num_epochs):
+            total_loss = 0
+            all_predictions = []
+            for i in range(0, data_size, batch_size):
+                if isinstance(data_X, pd.Series) or isinstance(data_X, pd.Series):
+                    batch_X = data_X[i:i+batch_size].values.reshape(-1, 1)  # Reshape to 2D array if data_X is 1D (pandas series is 1d)
+                else:
+                    batch_X = data_X[i:i+batch_size]
+                batch_y = data_y[i:i+batch_size]
+                batch_loss, batch_predictions = self.forward_and_backward_pass(layers, activations, batch_X, batch_y, learning_rate)
+                total_loss += batch_loss
+                all_predictions.append(batch_predictions)
+
+            # Combine predictions from all batches
+            predictions = np.vstack(all_predictions)
+            
+            # Calculate and print the average loss and accuracy for this epoch
+            average_loss = total_loss / (data_size / batch_size)
+            accuracy = np.mean(np.argmax(predictions, axis=1) == data_y) # Assuming data_y represents true class labels
+            print(f'Epoch {epoch + 1}/{num_epochs}, Loss: {average_loss}, Accuracy: {accuracy}')
+
+
+    def testing_model(self, layers, activations, data_X):
+        x = data_X
+
+        # Forward pass only for testig. We are not trying to update the model but simply test its acuracy
+        for layer, activation in zip(layers, activations):
+            layer.forward(x)
+            activation.forward(layer.output)
+            x = activation.output
+        
+        return x
+    
+
+'''
+def save_model(self):
+    model_data = {
+    'layer1_weights': self.layer1.weights,
+    'layer1_biases': self.layer1.biases,
+    'layer2_weights': self.layer2.weights,
+    'layer2_biases': self.layer2.biases,
+    'activation1_state': self.activation1.__dict__,
+    'activation2_state': self.activation2.__dict__,
+    }
+    print(model_data)
+'''
--- testing.py
+++ testing.py
@@ -1,74 +1,76 @@
 import numpy as np
 import pandas as pd
-from epoch import epoch_function
+from model.model import Model
 from create_layer import CreateLayer
-from dataset import generate_spiral_set
 from split_training_data import split_training_data
-from data_conversion import data_conversion
-from text_processing import text_processing
-
 
 '''
 This class is messy at the moment for testing purposes (but this needs to be cleaned a bit at one point)
 '''
 
+model = Model()
+
 
 #data = pd.read_csv('./dataset/random_int.csv', encoding='utf-8', dtype = str)  
-data = pd.read_csv('./dataset/wordlist_random.csv', encoding='utf-8', dtype = str)  
+data = pd.read_csv('./dataset/phonemes.csv', encoding='utf-8')  
+
 # Remove invalid rows
-#data = data.dropna(subset=['Int_value'])
-data = data.dropna(subset=['String'])
+#data = data.dropna(subset=['String'])
 # Shuffle the data if sample not shuffled already
 #data = data.sample(frac=1).reset_index(drop=True)
 
-
 #data = data[pd.to_numeric(data['Int_value'], errors='coerce').notna()]
 #data['Int_value'] = pd.to_numeric(data['Int_value'])
-#data_encoded = pd.get_dummies(data, columns=['String'])
 
-# Convert data to values between 0 and 1 with mean/max scaling or standardization 
-#data['Int_value_scaled'] = data_conversion.standardization(data, 'Int_value')
-tokenizer, tokenized_sentences = text_processing.create_tokenizer(data['String'])
+#tokenizer, tokenized_sentences = text_processing.create_tokenizer(data['String'])
 
-clean_token_data = data_conversion.token_to_int(tokenized_sentences)
-data['test'] = clean_token_data
-
-data['string_value_stand'] = data_conversion.standardization(data, 'test')
-
-data['length'] = data['String'].apply(len)
-data['alpha'] = data['String'].str.isalpha().astype(int)
-data['numeric'] = data['String'].str.isnumeric().astype(int)
-# 'Int_value' now contains numeric values
-#X = data['Int_value_scaled']
-X = data['string_value_stand']
-
-X = data[['length', 'alpha', 'numeric']]
-#X = data_encoded
-#label_mapping = {'small': 0, 'big': 1}
-label_mapping = {'str': 0, 'int': 1}
-#y = data['Size'].map(label_mapping).astype(int)
-y = data['Category'].map(label_mapping).astype(int)
+#clean_token_data = data_conversion.token_to_int(tokenized_sentences)
+#data['test'] = clean_token_data
 
 
+#data['string_value_stand'] = data_conversion.standardization(data, 'test')
+
+#data['length'] = data['String'].apply(len)
+#data['alpha'] = data['String'].str.isalpha().astype(int)
+#data['numeric'] = data['String'].str.isnumeric().astype(int)
+
+#X = data['string_value_stand']
+
+X = data[['V1', 'V2', 'V3', 'V4', 'V5']]
+
+#abel_mapping = {'1': 0, '2': 1}
+
+#y = data['Class'].map(label_mapping).astype(int)
+y = data['Class']
 #X, y = generate_spiral_set.create_data(100, 3)
 
 training_set_X, training_set_y, testing_set_X, testing_set_y = split_training_data(X, y, training_size=0.8)
 
+number_classes = 5
 # Hyperparameters
-learning_rate = 0.01
+learning_rate = 0.3
 num_epochs = 10  # Specify the number of training epochs
-batch_size = 16
+batch_size = 128
 
 # Number of classes = number of inputs
 # In case of second layer until the end it takes the number of neurons from the previous layer
 # Ex layer1(2,512), layer2(512,3) layer3(3, 128) etc.
-layer1 = CreateLayer.create(number_classes=3, number_neurons=16, activation_function='sigmoid')
-layer2 = CreateLayer.create(number_classes=16, number_neurons=2, activation_function='sigmoid')
-#layer3 = CreateLayer.create(number_classes=4, number_neurons=2, activation_function='softmax')
-layers=[layer1, layer2]
+layer1 = CreateLayer.create(number_classes=number_classes, number_neurons=32, activation_function='relu')
+layer2 = CreateLayer.create(number_classes=32, number_neurons=16, activation_function='relu')
+layer3 = CreateLayer.create(number_classes=16, number_neurons=5, activation_function='softmax')
+layers=[layer1, layer2, layer3]
 
 layer_dense_list = [layer[0] for layer in layers]
 activation_layer_list = [layer[1] for layer in layers]
 
-epoch_function(layers = layer_dense_list, activations=activation_layer_list, batch_size=batch_size, 
+model.train_model(layers = layer_dense_list, activations=activation_layer_list, batch_size=batch_size, 
                num_epochs=num_epochs, learning_rate=learning_rate,data_X=training_set_X, data_y=training_set_y)
+
+predictions = model.testing_model(layers=layer_dense_list, activations=activation_layer_list, data_X=testing_set_X)
+
+# For binary classification, the prediction is the index of the maximum value in the last layer's output
+    # /!\ need to have something for more than binary classification
+predicted_classes = np.argmax(predictions, axis=1)
+
+accuracy = np.mean(predicted_classes == testing_set_y)
+print(f"Test accuracy: {accuracy}")
